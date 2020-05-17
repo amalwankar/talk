b417581467b7fe26fdc2232a52c48d32401e5cd6
Abhishek D Malwankar <amalwankar@outlook.com>
2020-05-16 22:01:34 -0500

    Made some changes

--- go.mod
+++ go.mod
@@ -2,4 +2,4 @@ module github.com/amalwankar/talk
 
 go 1.13
 
-require rsc.io/quote v1.5.2 // indirect
+require rsc.io/quote v1.5.2
--- talk.go
+++ talk.go
@@ -2,16 +2,14 @@ package talk
 
 import (
 	"fmt"
-
-	"rsc.io/quote"
 )
 
 func Say(something string) string {
-	val := fmt.Sprintf("Hello from speak %s", something)
+	val := fmt.Sprintf("Hello from talk %s", something)
 
 	return val
 }
 
 func Quote() string {
-	return quote.Hello()
+	return "test" //quote.Hello()
 }
